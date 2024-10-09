5edee2907945f659aa24d52c87f901734c5edc9e
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-10-09 23:55:46 +0800

    build: add heroku-postgre

--- canvas/settings.py
+++ canvas/settings.py
@@ -153,9 +153,9 @@ DATABASES = {
     }
 }
 
-# db_from_env = dj_database_url()
-# DATABASES["default"].update(db_from_env)
-# DATABASES["default"]["CONN_MAX_AGE"] = 500
+db_from_env = dj_database_url()
+DATABASES["default"].update(db_from_env)
+DATABASES["default"]["CONN_MAX_AGE"] = 500
 
 # Use production database
 # if not DEBUG and ENVIRONMENT == Env.PRODUCTION:
